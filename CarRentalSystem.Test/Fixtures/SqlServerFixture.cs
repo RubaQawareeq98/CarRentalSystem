15d2667f9b39b65f8153af41fed22128b52be7f4
ruba <s12028675@stu.najah.edu>
2025-05-24 15:12:59 +0300

    extract image & password to appsettings.json file

--- CarRentalSystem.Test/Fixtures/SqlServerFixture.cs
+++ CarRentalSystem.Test/Fixtures/SqlServerFixture.cs
@@ -2,6 +2,7 @@ using CarRentalSystem.Api;
 using CarRentalSystem.Db;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Testcontainers.MsSql;
 
@@ -9,13 +10,27 @@ namespace CarRentalSystem.Test.Fixtures;
 
 public class SqlServerFixture : IAsyncLifetime
 {
-    private readonly MsSqlContainer _dbContainer = new MsSqlBuilder()
-        .WithImage("mcr.microsoft.com/mssql/server:2022-latest")
-        .WithPassword("Ruba*12345")
-        .Build();
+    private readonly MsSqlContainer _dbContainer;
 
     public HttpClient Client { get; private set; }
     public string ConnectionString { get; private set; }
+
+    public SqlServerFixture()
+    {
+        IConfiguration configuration =
+            new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false)
+            .Build();
+
+        var image = configuration["SqlServerContainer:Image"];
+        var password = configuration["SqlServerContainer:Password"];
+
+        _dbContainer = new MsSqlBuilder()
+            .WithImage(image)
+            .WithPassword(password)
+            .Build();
+    }
 
     public async Task InitializeAsync()
     {
