c09b5d6fd4bd1f192bab0cf4d25957e8fb335f55
ruba <s12028675@stu.najah.edu>
2025-05-26 13:00:26 +0300

    Refactor test

--- CarRentalSystem.Test/Controllers/CarControllerTest.cs
+++ CarRentalSystem.Test/Controllers/CarControllerTest.cs
@@ -111,13 +111,22 @@ public class CarControllerTest : IClassFixture<SqlServerFixture>
     public async Task GetSearchCars_WithFilters_ShouldReturnsFilteredCars()
     {
         // Arrange
+        var searchDto = _fixture.Build<CarSearchDto>()
+            .With(s => s.StartDate, DateTime.Today)
+            .With(s => s.EndDate, DateTime.Today.AddDays(3))
+            .With(s => s.MinPrice, 10000m)
+            .With(s => s.MaxPrice, 20000m)
+            .With(s => s.MinYear, 2020)
+            .With(s => s.MaxYear, 2023) 
+            .Create();
+        
         var redCars = _fixture.Build<Car>()
-            .With(c => c.Color, "Red")
-            .With(c => c.Brand, "Toyota")
-            .With(c => c.Model, "Corolla")
-            .With(c => c.Location, "New York")
-            .With(c => c.Price, 15000m)
-            .With(c => c.Year, 2020)
+            .With(c => c.Color, searchDto.Color)
+            .With(c => c.Brand, searchDto.Brand)
+            .With(c => c.Model, searchDto.Model)
+            .With(c => c.Location, searchDto.Location)
+            .With(c => c.Price, searchDto.MinPrice)
+            .With(c => c.Year, searchDto.MinYear)
             .With(c => c.IsAvailable, true)
             .Without(c => c.Reservations) 
             .CreateMany(2)
@@ -131,25 +140,11 @@ public class CarControllerTest : IClassFixture<SqlServerFixture>
         TestAuthenticationHeader.SetTestAuthHeader(_client, _fixture.Create<Guid>(), UserRole.Customer);
 
         await CarTestUtilities.AddTestCars(redCars.Concat(cars).ToList(), _factory);
-        
-        var searchDto = new CarSearchDto
-        {
-            Color = "Red",
-            Brand = "Toyota",
-            MinYear = 2019,
-            MaxYear = 2025,
-            Model = "Corolla",
-            Location = "New York",
-            MinPrice = 14000,
-            MaxPrice = 16000,
-            StartDate = DateTime.Today,
-            EndDate = DateTime.Today.AddDays(3)
-        };
 
         // Act
         var query = $"""
-                     ?Color={searchDto.Color}&Brand={searchDto.Brand}&MinYear={searchDto.MinYear}&Model={searchDto.Model}&
-                         Location={searchDto.Location}&MinPrice={searchDto.MinPrice}&MaxPrice={searchDto.MaxPrice}&StartDate={searchDto.StartDate}&EndDate={searchDto.EndDate}
+                     ?Color={searchDto.Color}&Brand={searchDto.Brand}&MinYear={searchDto.MinYear}&MaxYear={searchDto.MaxYear}&Model={searchDto.Model}&
+                     Location={searchDto.Location}&MinPrice={searchDto.MinPrice}&MaxPrice={searchDto.MaxPrice}&StartDate={searchDto.StartDate}&EndDate={searchDto.EndDate}
                      """;
         var response = await _client.GetAsync($"{BaseUrl}/search{query}");
 
